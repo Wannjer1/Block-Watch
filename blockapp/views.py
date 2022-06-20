b466fb61f4e550eeaa4a1e8152ed5f688cf47a0d
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-20 19:32:13 +0300

    bg-bug fix neighbourhoods display

--- blockapp/views.py
+++ blockapp/views.py
@@ -12,7 +12,7 @@ def home(request):
 
 
 # function to add neighbourhood
-def AddNeighbourhood(request, username):
+def AddNeighbourhood(request):
     # remember to link this function to the profile model
     if request.method == 'POST':
         form = AddNeighbourhoodForm(request.POST, request.FILES)
@@ -21,13 +21,22 @@ def AddNeighbourhood(request, username):
             neighbourhood.neighbourhood_admin = request.user
             neighbourhood.save()
             messages.success(request, 'A Neighbourhood Was Created Successfully!')
-            return redirect('MyNeighbourhoods', username=username)
+            return redirect('MyNeighbourhoods')
         else:
             messages.error(request, "A Neighbourhood Wasn't Created!")
             return redirect('AddNeighbourhood')
     else:
         form = AddNeighbourhoodForm()
-    return render(request, 'AddNeighbourhood.html', {'form':form})
+    return render(request, 'blockapp/addhood.html', {'form':form})
+
+# function to show users neighbourhood
+def MyNeighbourhoods(request):
+#    remember to link with the profile model
+    neighbourhoods = NeighbourHood.objects.all()
+    for neighbourhood in neighbourhoods:
+        print(neighbourhood.title)
+        print(neighbourhood.description)
+    return render(request, 'blockapp/neighbourhoods.html', {'neighbourhoods':neighbourhoods})
 
 # funtion to create a new post
 def new_post(request):
@@ -44,8 +53,15 @@ def new_post(request):
         form = NewPostForm()
     return render(request, 'blockapp/addpost.html', {'form': form,'current_user': current_user}) 
 
+# function to view users posts 
+def MyPosts(request):
+    posts = Post.objects.filter.all()
+    return render(request, 'blockapp/posts.html', {'posts':posts})
+
+
 # function to add business
 def AddBusiness(request):
+    # remember to add profile model
     form = BusinessForm()
     if request.method == 'POST':
         form = BusinessForm(request.POST)
@@ -65,3 +81,25 @@ def AddBusiness(request):
         form = BusinessForm()
     
     return render(request, 'blockapp/addbiz.html', {'form': form,})
+
+# function to show users business post
+def MyBusinesses(request):
+#    remember to link this with the user profile
+  
+    businesses = Business.objects.filter.all()
+    return render(request, 'blockapp/business.html', {'businesses':businesses})
+
+# function to enable users search for posted businesses
+def Search(request):
+    if request.method == 'POST':
+        search = request.POST['BusinessSearch']
+        print(search)
+        businesses = Business.objects.filter(name__icontains = search).all()
+        return render(request, 'blockapp/search.html', {'search':search, 'businesses':businesses})
+    else:
+        return render(request, 'blockapp/search.html')
+
+
+
+
+
