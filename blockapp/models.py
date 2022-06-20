ff1423e68d2247db72347a3e0a5884d015d04dc6
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-20 09:28:34 +0300

    created post model

--- blockapp/models.py
+++ blockapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to = 'post_img/')
+    description = models.TextField(blank=True, max_length=255)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
+    post_date = models.DateTimeField(auto_now_add=True, null=True)
+
+    def save_post(self):
+        self.save()
+
+    
