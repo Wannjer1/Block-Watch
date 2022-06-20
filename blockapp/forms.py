e4eb39f5749867e155d5fb75f0c5ca3bc7e7eae9
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-20 15:20:04 +0300

    created neighbourhood form

--- blockapp/forms.py
+++ blockapp/forms.py
@@ -14,6 +14,20 @@ CATEGORIES = [
     ('7', 'Event'),
 ]
 
+# neighbourhood form
+class AddNeighbourhoodForm(forms.ModelForm):
+    title = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Title','class': 'form-control mb-4'}))
+    description = forms.CharField(required=True, widget=forms.Textarea(attrs={'placeholder': 'Description','class': 'form-control mb-4','rows': 3,}))
+    location = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Location','class': 'form-control mb-4'}))
+    neighbourhood_logo = forms.ImageField(required=True, widget=forms.FileInput(attrs={'class': 'dropify', 'data-height':300, 'data-max-file-size':"1M"}))
+    health_department = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Health Department','class': 'form-control mb-4'}))
+    police_department = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Police Department','class': 'form-control mb-4'}))
+
+    class Meta:
+        model = NeighbourHood
+        fields = ('title','description','location','neighbourhood_logo','health_department','police_department')
+
+
 # new post form 
 class NewPostForm(forms.Form):
     title = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control mb-4', 'placeholder':'Post Title'}))
